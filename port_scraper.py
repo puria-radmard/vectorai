7039633f87d5fd10e6129530ba84a95000c50a2c
Puria Radmard <pr450@cam.ac.uk>
2019-12-20 13:34:59 +0000

    added port and vessel scrapers

---
+++ port_scraper.py
@@ -0,0 +1,24 @@
+# In this file, I've taken the map embedded on this page: https://directories.lloydslist.com/ 
+# and taken all the markers listed on its associated .json file to a list
+
+# On the page source, the map can be found at .html address: 
+# /html/body/div/div[2]/div[2]/div/div[2]/div/div[3]/div/div/div/form/div[2]/div/script/text()
+
+# From here, we learn that the marker information on their maps is from the JSON
+# https://directories.lloydslist.com/script/stockist_map_details.json
+
+# Now we can parse the JSON for the name of every marker
+
+import json
+from urllib.request import urlopen
+
+url = "https://directories.lloydslist.com/script/stockist_map_details.json"
+response = urlopen(url).read()
+data = json.loads(response.decode('utf-8'))
+#print(str(data)[:100])                                        # The JSON was weirdly split in two, and the terminal only displayed the last portion of the source, so I had to inspect what the opening keyword was
+
+outlist = []
+for place in data["places"]:
+    outlist.append(str(place["title"]))
+
+print(outlist)
---
+++ vessel_scraper.py
@@ -0,0 +1,62 @@
+# This file aims to scrape for names of vessels from:
+# https://www.marinetraffic.com/en/data/?asset_type=vessels&columns=flag,shipname,photo,recognized_next_port,reported_eta,reported_destination,current_port,imo,ship_type,show_on_live_map,time_of_latest_position,lat_of_latest_position,lon_of_latest_position
+
+# To make it easier for myself, I reduced the number of columsn by going to:
+# https://www.marinetraffic.com/en/data/?asset_type=vessels&columns=shipname
+# which gives just the one column table of ship names
+
+# (I tried to just click export data at the top, but this cost money)
+
+# The issue with this page is that the link does not change when you click next page, so a web driver like Selenium has to be used
+
+# I then realised that the page given only provides 20*25 = 500 ships, so I instead used:
+# https://www.fleetmon.com/vessels/
+# for scraping
+
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+
+def scrapeWithScript(url, xpath, iteratorID):
+
+    outlist = []
+
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get(url)
+    n = 0
+
+    TIMEOUT  = 5
+    LOADING_ELEMENT_XPATH = "vesselsearch_table_processing"
+
+    while True:
+        for elem in driver.find_elements_by_xpath("/html/body/main/div/div/table/tbody/tr/td/div/div/strong/a"):
+            print(outlist, n, len(outlist))
+            if elem.text not in outlist: outlist.append(elem.text)
+            n+=1
+        
+        try:
+
+            elem = driver.find_element_by_id(iteratorID)
+            ActionChains(driver).move_to_element(elem).click(elem).perform()
+            #time.sleep(2)
+            
+            WebDriverWait(driver, TIMEOUT).until(EC.presence_of_element_located((By.XPATH, LOADING_ELEMENT_XPATH)))
+            WebDriverWait(driver, TIMEOUT).until_not(EC.presence_of_element_located((By.XPATH, LOADING_ELEMENT_XPATH)))
+
+        except:                 # Sometimes you get hit with an advert, and you can just click back page and resume to the same page
+            driver.get(url)
+
+    driver.quit()
+    print(outlist)
+
+scrapeWithScript(url = "https://www.fleetmon.com/vessels/",
+                 xpath =  "/html/body/main/div/div/table/tbody/tr/td/div/div/strong/a",
+                 iteratorID = "vesselsearch_table_next")
+
+                 
