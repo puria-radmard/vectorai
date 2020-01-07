9b774d7dd3d0a7915d83f7a277aaf4d3ee498917
Puria Radmard <pr450@cam.ac.uk>
2020-01-07 16:46:18 +0000

    tried running on azure but need some help troubleshooting

--- training_model.py
+++ training_model.py
@@ -19,26 +19,28 @@ num_filters=128
 
 textRNN=TextCNN(filter_sizes,num_filters,num_classes, learning_rate, batch_size, decay_steps, decay_rate,sequence_length,embed_size,is_training)
 
-n_epochs = 8
+n_epochs = 1
 saver = tf.train.Saver()
 
+def train_and_save():
+    with tf.Session() as sess:
+        
+        sess.run(tf.global_variables_initializer())
 
-with tf.Session() as sess:
-    
-    sess.run(tf.global_variables_initializer())
+        for n in range(n_epochs):
 
-    for n in range(n_epochs):
+            for i in range(len(X_train) % batch_size):
 
-        for i in range(len(X_train) % batch_size):
+                input_x = np.stack(X_train.iloc[i*batch_size : batch_size*(i+1)].apply(em_aug).values)         # New augmentation every time
+                input_y = y_train.iloc[i*batch_size : batch_size*(i+1)].values
+                input_y = np.vectorize(dictionary.get)(input_y).reshape(-1)
+                loss,possibility,W_projection_value,_=sess.run([textRNN.loss_val,textRNN.possibility,textRNN.W_projection,textRNN.train_op],
+                                                        feed_dict={textRNN.X_in:input_x,textRNN.y_in:input_y,
+                                                                    textRNN.dropout_keep_prob:dropout_keep_prob,textRNN.tst:False,
+                                                                    textRNN.is_training_flag:is_training})
 
-            input_x = np.stack(X_train.iloc[i*batch_size : batch_size*(i+1)].apply(em_aug).values)         # New augmentation every time
-            input_y = y_train.iloc[i*batch_size : batch_size*(i+1)].values
-            input_y = np.vectorize(dictionary.get)(input_y).reshape(-1)
-            loss,possibility,W_projection_value,_=sess.run([textRNN.loss_val,textRNN.possibility,textRNN.W_projection,textRNN.train_op],
-                                                    feed_dict={textRNN.X_in:input_x,textRNN.y_in:input_y,
-                                                                textRNN.dropout_keep_prob:dropout_keep_prob,textRNN.tst:False,
-                                                                textRNN.is_training_flag:is_training})
+            print("Batch {} of epoch {} completed, loss = {}".format(i, n, loss))
+        
+        save_patch = saver.save(sess, "./vectorai_word_classifier.ckpt")
 
-        print("Batch {} of epoch {} completed, loss = {}".format(i, n, loss))
-    
-    save_patch = saver.save(sess, "./vectorai_word_classifier.ckpt")
+train_and_save()
